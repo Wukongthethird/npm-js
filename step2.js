becf713e6c95d79bce5153cda1da27aa9c9f5bc1
Nathaniel Cuenca <ncuenca.nac@gmail.com>
2021-07-27 12:24:32 -0700

    finished step 2

--- step2.js
+++ step2.js
@@ -1,21 +1,34 @@
 "use strict"
 
 const fsP = require("fs/promises");
-
+const axios = require("axios");
 const argv = process.argv;
 
 async function cat(filename) {
   try {
     let contents = await fsP.readFile(`${filename}`, "utf8");
-    console.log("file contents", contents);
+    console.log("file contents:", contents);
   } catch (err) {
+    console.log(`Error fetching ${filename}:
+            ${filename} isn't a valid path`);
     process.exit(1);
   }
 }
 
 async function webCat(url) {
-    
+    try {
+        const resp = await axios.get(url);
+        console.log(resp.data);
+    } catch (err) {
+        console.log(`Error fetching ${url}:
+            ${url} isn't a valid url`);
+        process.exit(1);
+    }
 }
 
-cat( argv[2] )
+if (argv[2].startsWith('http')) {
+    webCat(argv[2]);
+} else {
+    cat(argv[2]);
+}
 
